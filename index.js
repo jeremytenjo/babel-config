716a647cf535edf3ced4da0e526cc547e03d3259
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2020-04-13 11:17:10 -0400

    removed @babel/plugin-transform-regenerator

--- index.js
+++ index.js
@@ -4,22 +4,21 @@ module.exports = {
       require('@babel/preset-env'),
       {
         targets: 'defaults, not ie <= 11, not edge > 0, not IE_Mob 11',
-        modules: false
-      }
+        modules: false,
+      },
     ],
-    require('@babel/preset-react')
+    require('@babel/preset-react'),
   ],
   plugins: [
     require('react-hot-loader/babel'),
-    require('@babel/plugin-transform-regenerator'),
     require('@babel/plugin-syntax-dynamic-import'),
     require('@babel/plugin-syntax-throw-expressions'),
     require('@babel/plugin-transform-runtime'),
-    require('babel-plugin-styled-components')
+    require('babel-plugin-styled-components'),
   ],
   env: {
     production: {
-      plugins: [require('babel-plugin-transform-react-remove-prop-types')]
-    }
-  }
+      plugins: [require('babel-plugin-transform-react-remove-prop-types')],
+    },
+  },
 }
